4f030ef7a2e3e653d10e070b65afa0725220f0ca
Jonathan Hogervorst <jonathan@buzzera.com>
2013-11-11 12:04:30 +0100

    Plist extraction separated; `plutil` is now optional

--- track-down-app-purchases.sh
+++ track-down-app-purchases.sh
@@ -35,11 +35,30 @@ function print_row
 	printf "\n"
 }
 
-function extract_key
+function plutil_installed
+{
+	hash plutil 2> /dev/null
+}
+
+function extract_plist_key
 {
 	local KEY=$1
-	local METADATA=$2
-	echo "$METADATA" | sed -e "/<key>$KEY<\\/key>/I,/<string>/I!d" | sed -En "s/^.*<string>(.+)<\\/string>.*$/\\1/Ip"
+	local PLIST=$2
+	echo "$PLIST" | sed -e "/<key>$KEY<\\/key>/I,/<string>/I!d" | sed -En "s/^.*<string>(.+)<\\/string>.*$/\\1/Ip"
+}
+
+function extract_plist
+{
+	local PLIST_NAME=$1
+	local ZIP_NAME=$2
+	local UNZIP_COMMAND="unzip -p \"$ZIP_NAME\" \"$PLIST_NAME\" 2> /dev/null"
+	
+	if plutil_installed
+	then
+		eval "$UNZIP_COMMAND" | plutil -convert xml1 -o - -
+	else
+		eval "$UNZIP_COMMAND"
+	fi
 }
 
 echo ""
@@ -63,14 +82,14 @@ print_row "$DASHES" "$DASHES" "$DASHES"
 
 for APP_FILE in $APP_FILES
 do
-	APP_FILE_METADATA=$( unzip -p "$APP_FILE" iTunesMetadata.plist 2> /dev/null | plutil -convert xml1 -o - - )
-	APP_FILE_APPLE_ID=$( extract_key "appleId" "$APP_FILE_METADATA" )
-	APP_FILE_BUNDLE_DISPLAY_NAME=$( extract_key "bundleDisplayName" "$APP_FILE_METADATA" )
-	APP_FILE_ITEM_NAME=$( extract_key "itemName" "$APP_FILE_METADATA" )
+	APP_PLIST=$( extract_plist "iTunesMetadata.plist" "$APP_FILE" )
+	APP_APPLE_ID=$( extract_plist_key "appleId" "$APP_PLIST" )
+	APP_BUNDLE_DISPLAY_NAME=$( extract_plist_key "bundleDisplayName" "$APP_PLIST" )
+	APP_ITEM_NAME=$( extract_plist_key "itemName" "$APP_PLIST" )
 	
-	if [ ! -z "$APP_FILE_APPLE_ID$APP_FILE_BUNDLE_DISPLAY_NAME$APP_FILE_ITEM_NAME" ]
+	if [ ! -z "$APP_APPLE_ID$APP_BUNDLE_DISPLAY_NAME$APP_ITEM_NAME" ]
 	then
-		print_row "$APP_FILE_ITEM_NAME" "$APP_FILE_BUNDLE_DISPLAY_NAME" "$APP_FILE_APPLE_ID"
+		print_row "$APP_ITEM_NAME" "$APP_BUNDLE_DISPLAY_NAME" "$APP_APPLE_ID"
 	else
 		APP_FILE_NAME=$( basename "$APP_FILE" )
 		INVALID_APP_FILES="• $INVALID_APP_FILES$APP_FILE_NAME\n"
@@ -85,6 +104,20 @@ then
 	echo "Some files could not be processed:"
 	printf "$INVALID_APP_FILES"
 	echo ""
+	
+	if ! plutil_installed
+	then
+		echo "You could try to install the Xcode Command Line Tools. It includes a utility"
+		echo "that can convert binary plist files to their XML equivalent. In short, it might"
+		echo "help this script process more of your apps."
+		echo ""
+		echo "Start by installing Xcode from the Mac App Store (for free). Then, go to the"
+		echo "Downloads tab within the Xcode Preferences menu and click \"Install\" next to the"
+		echo "Command Line Tools entry."
+		echo ""
+		echo "More information and source: https://developer.apple.com/support/xcode/"
+		echo ""
+	fi
 fi
 
 IFS="$OIFS"
